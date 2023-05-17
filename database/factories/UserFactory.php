f25fc9997c2c666ba0fcdd1d603706b2a38c0e2a
rdzPedraos <juancursosrodriguez@gmail.com>
2023-05-17 00:35:36 -0500

    updated userFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,7 +19,11 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            'document_number' => fake()->unique()->randomNumber(10),
             'email' => fake()->unique()->safeEmail(),
+            'country' => 'col',
+            'phone_number' => fake()->randomNumber(10, true),
+
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
