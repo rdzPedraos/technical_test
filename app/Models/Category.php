70bc838f29598ca755b33bc9f01b465d978856b4
rdzPedraos <juancursosrodriguez@gmail.com>
2023-05-17 01:07:35 -0500

    create relation many to many between Category and User models

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
 }
